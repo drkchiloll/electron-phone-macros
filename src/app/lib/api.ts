88ec5b691fc4b6a00f61c4b71827fd96dc60cf23
drkchiloll <samwomack_@me.com>
2017-10-09 05:03:46 -0500

    modified structure + methods

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,20 +1,88 @@
-declare module NodeJS {
-  interface Global {
-    myModule:any;
+// import * as Promise from 'bluebird';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as Datastore from 'nedb-core';
+
+const appOut = path.resolve(__dirname, '../../'),
+      appIn = path.resolve(__dirname, '../');
+
+export class Api {
+  acctDb: Datastore;
+  queryDb: Datastore;
+  editorDb: Datastore;
+  recordId: string;
+
+  constructor({db, dbName}:any) {
+    let filename: string;
+    if(fs.existsSync(`${appOut}/.data`)) {
+      filename = `${appOut}/.data/.${dbName}`;
+    } else {
+      filename = `${appIn}/.data/.${dbName}`;
+    }
+    this.setDB(db, filename);
+  }
+
+  setDB(db: string, filename: string) {
+    switch(db) {
+      case 'acctDb':
+        this.acctDb = new Datastore({ filename, autoload: true });
+        break;
+      default:
+        return null;
+    }
+    return db;
+  }
+
+  whichDB() {
+    if(this.acctDb) return this.acctDb;
+    if(this.queryDb) return this.queryDb;
+    if(this.editorDb) return this.editorDb;
+  }
+
+  get(query={}) {
+    return new Promise((resolve, reject) => {
+      let db = this.whichDB();
+      db.find(query, (err: any, docs: any) => resolve(docs));
+    });
+  }
+
+  add(record:any) {
+    return new Promise((resolve, reject) => {
+      let db = this.whichDB();
+      db.insert(record, (err, doc) => resolve(doc));
+    });
+  }
+
+  update(record:any) {
+    let _id = JSON.parse(JSON.stringify(record))._id;
+    return new Promise((resolve, reject) => {
+      let db = this.whichDB();
+      db.update({ _id }, record, { upsert: false },
+        (err:any, num:number) => resolve(num));
+    });
+  }
+
+  remove(_id:string) {
+    return new Promise((resolve, reject) => {
+      let db = this.whichDB();
+      db.remove({ _id }, {}, (err: any, num: any) => {
+        return resolve(num);
+      });
+    });
   }
 }
-global.myModule = (function() {
-  let service:any = {};
 
-  service.init = function() {
-    console.log('I initialized');
-    let java = require('java');
-    java.newInstance("java.util.ArrayList", function(err:any, list:any) {
-      list.addSync("item1");
-      list.addSync("item2");
-      console.log(list.toStringSync()); // [item1, item2]
-    });
-  };
+export interface IAccount {
+  name:string;
+  host:string;
+  version:string;
+  username:string;
+  password:string;
+  selected:boolean;
+  connected:boolean;
+}
 
-  return service;
-})();
+export interface IEditorSettings {
+  vimMode:boolean;
+  fontSize:number;
+}

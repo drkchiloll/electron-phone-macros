b1907989a0eadd3c38fc8ca940d3813b80b15d8e
drkchiloll <samwomack_@me.com>
2018-06-27 13:14:00 -0500

    implemented for MacroBuilder

--- src/app/lib/phone-macros.ts
+++ src/app/lib/phone-macros.ts
@@ -9,6 +9,33 @@ export const phone = (() => {
       thumb: '//ImageItem/@Image',
       image: '//ImageItem/@URL'
     },
+    devices: {
+      6900: ['6921', '6941', '6945', '6961'],
+      7800: ['7811', '7821', '7841', '7861'],
+      7900: [
+        '7941',
+        '7942',
+        '7945',
+        '7961',
+        '7962',
+        '7965',
+        '7970',
+        '7971',
+        '7975'
+      ],
+      8800: [
+        '8811',
+        '8841',
+        '8845',
+        '8851',
+        '8861',
+        '8865',
+        '8831',
+        '8832'
+      ],
+      8900: ['8941', '8945', '8961'],
+      9900: ['9951', '9971']
+    },
     itl() {
       return [{
         type: '8800',
@@ -99,6 +126,190 @@ export const phone = (() => {
         method: 'get',
         responseType: 'arraybuffer'
       }).then(resp => resp.data);
-    }
+    },
+    cmds: {
+      keys: [{
+        name: 'Applications',
+        displayName: 'Press the Applications Key'
+      }, {
+        name: 'KeyPad0',
+        displayName: 'Press the 0 key'
+      }, {
+        name: 'KeyPad1',
+        displayName: 'Press the 1 key'
+      }, {
+        name: 'KeyPad2',
+        displayName: 'Press the 2 key'
+      }, {
+        name: 'KeyPad3',
+        displayName: 'Press the 3 key'
+      }, {
+        name: 'KeyPad4',
+        displayName: 'Press the 4 key'
+      }, {
+        name: 'KeyPad5',
+        displayName: 'Press the 5 key'
+      }, {
+        name: 'KeyPad6',
+        displayName: 'Press the 6 key'
+      }, {
+        name: 'KeyPad7',
+        displayName: 'Press the 7 key'
+      }, {
+        name: 'KeyPad8',
+        displayName: 'Press the 8 key'
+      }, {
+        name: 'KeyPad9',
+        displayName: 'Press the 9 key'
+      }, {
+        name: 'KeyPadPound',
+        displayName: 'Press the # key'
+      }, {
+        name: 'KeyPadStar',
+        displayName: 'Press the * key'
+      }, {
+        name: 'Soft1',
+        displayName: 'Press SoftKey 1'
+      }, {
+        name: 'Soft2',
+        displayName: 'Press SoftKey 2'
+      }, {
+        name: 'Soft3',
+        displayName: 'Press SoftKey 3'
+      }, {
+        name: 'Soft4',
+        displayName: 'Press SoftKey 4'
+      }, {
+        name: 'Soft5',
+        displayName: 'Press SoftKey 5'
+      }, {
+        name: 'Speaker',
+        displayName: 'Press Speaker Button'
+      }, {
+        name: 'VolUp',
+        displayName: 'Press Volume Up'
+      }, {
+        name: 'VolDwn',
+        displayName: 'Press Volume Down'
+      }, {
+        name: 'Hold',
+        displayName: 'Press Hold Button'
+      }, {
+        name: 'Directories',
+        displayName: 'Press Directories Key'
+      }, {
+        name: 'Mute',
+        displayName: 'Press Mute Key'
+      }, {
+        name: 'Onhook',
+        displayName: 'Press Onhook Key'
+      }, {
+        name: 'Offhook',
+        displayName: 'Press Offhook Key'
+      }, {// Hold for 69/89/99
+        name: 'FixedFeature3',
+        displayName: 'Hold Key for 69/89/9900 Series Phones'
+      }, {
+        name: 'Settings',
+        displayName: 'Press Settings Key'
+      }],
+      init: {
+        name: 'Services',
+        displayName: 'Init: Clears Display (out of Menus etc)'
+      }
+    },
+    commands(deviceType) {
+      let keys: any;
+      switch(deviceType) {
+        case '6900':
+          keys = this.cmds.keys
+            .reduce((a: any, c: any) => {
+              switch(c.name) {
+                case 'Applications':
+                case 'Hold':
+                case 'Directories':
+                  return a;
+                default:
+                  c['type'] = 'key';
+                  a.push(c);
+                  return a;
+              }
+            }, [])
+          return keys.concat([{ type: 'init', ...this.cmds.init }]);
+        case '7900':
+          keys = this.cmds.keys
+            .reduce((a: any, c: any) => {
+              switch(c.name) {
+                case 'Applications':
+                case 'Onhook':
+                case 'Offhook':
+                  return a;
+                default:
+                  c['type'] = 'key';
+                  a.push(c);
+                  return a;
+              }
+            }, []);
+          return keys.concat([{ type: 'init', ...this.cmds.init }]);
+        case '7800':
+          keys = this.cmds.keys
+            .reduce((a: any, c: any) => {
+              switch(c.name) {
+                case 'Onhook':
+                case 'Offhook':
+                case 'Soft5':
+                  return a;
+                default:
+                  c['type'] = 'key';
+                  a.push(c);
+                  return c;
+              }
+            }, [])
+          return keys.concat([{ type: 'init', ...this.cmds.init }]);
+        case '8800':
+          return this.cmds.keys
+            .reduce((a: any, c: any) => {
+              switch(c.name) {
+                case 'Onhook':
+                case 'Offhook':
+                  return a;
+                default:
+                  c['type'] = 'key';
+                  a.push(c);
+                  return c;
+              }
+            }, [])
+        case '8900':
+          return this.cmds
+          .reduce((a: any, c: any) => {
+            switch(c.name) {
+              case 'Directories':
+              case 'Hold':
+              case 'Onhook':
+              case 'Offhook':
+                return a;
+              default:
+                c['type'] = 'key';
+                a.push(c);
+                return c;
+            }
+          }, [])
+        case '9900':
+          return this.cmds
+          .reduce((a: any, c: any) => {
+            switch(c.name) {
+              case 'Directories':
+              case 'Hold':
+              case 'Onhook':
+              case 'Offhook':
+                return a;
+              default:
+                c['type'] = 'key';
+                a.push(c);
+                return c;
+            }
+          }, [])
+      }
+    },
   };
 })();

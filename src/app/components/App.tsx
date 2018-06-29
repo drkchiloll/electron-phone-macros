3ffbc5f499beeba98a2bd16fa651985f22e717ad
drkchiloll <samwomack_@me.com>
2018-06-29 06:46:20 -0500

    refactor

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,6 +4,9 @@ import {
   Component
 } from './index';
 import { PhoneMacros } from './PhoneMacros';
+import { Backgrounds } from './Backgrounds';
+import { modelDb } from '../lib/model-db';
+import { accountDb } from '../lib/account-db';
 
 export class App extends Component<any, any> {
   constructor() {
@@ -12,37 +15,15 @@ export class App extends Component<any, any> {
       tabValue: 'mainView',
       openAcct: false,
       tabIndx: 1,
-      db: null,
-      api: null,
-      modelNum: null,
-      account: []
+      account: null
     };
   }
   componentWillMount() {
-    let api = new Api({ db: 'acctDb', dbName: 'accounts' }),
-        modelDb = new Api({ db: 'modelDb', dbName: 'models' }),
-        account:any;
-    this.setState({ api });
-    return modelDb.get().then((recs:any) => {
-      return api.get().then((accounts: any) => {
-        if(accounts.length > 0) {
-          account = accounts.find((r: any) => r.selected);
-        }
-        this.setState({ modelNum: recs, account });
-        if(recs.length === 0) return account; 
-        else return;
-      });
-    }).then((acct) => {
-      if(acct && acct.host) {
-        let cucm = new Cucm(acct);
-        return cucm.query(phModelQuery, true);
-      }
-    }).then((results:any) => {
-      // console.log(results);
-      if(results && results.length > 0) {
-        modelDb.add(results).then((resp: any) => {
-          console.log(resp);
-        });
+    let account:any;
+    accountDb.get().then((accounts: any) => {
+      if(accounts.length > 0) {
+        account = accounts.find((r: any) => r.selected);
+        this.setState({ account });
       }
     });
   }
@@ -61,49 +42,58 @@ export class App extends Component<any, any> {
     });
   }
   render() {
+    const { account, openAcct, tabValue } = this.state;
     return (
-      <div>
-        <div style={{width: 405, marginBottom: 5}}>
-          <Tabs className='tabs-container'
-            inkBarStyle={{ background: 'black' }}
-            tabItemContainerStyle={{ width: 405 }}
-            initialSelectedIndex={this.state.tabIdx}
-            value={this.state.tabValue}
-            onChange={this._tabSelect}>
-            <Tab icon={
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-server fa-lg'/>
-                </span>
-              }
-              label='Accounts'
-              value='accounts'>
-              <Accounts api={this.state.api}
-                openDia={this.state.openAcct}
-                acctClose={this._handleClose} />
-            </Tab>
-            <Tab icon={
-              <span className='fa-stack fa-lg'>
-                <i className='fa fa-phone fa-lg' />
-              </span>
-            }
-              label='Device Search'
-              value='device-search' >
-              <MainView api={this.state.api}
-                modelNum={this.state.modelNum} />
-            </Tab>
-            <Tab icon={
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-superpowers fa-lg' />
-                </span>
-              }
-              label='Phone Macros'
-              value='templates' >
-              <PhoneMacros account={this.state.account} />
-            </Tab>
-          </Tabs>
-          {/* <hr color='black' style={{ width: 180 }} /> */}
-        </div>
-      </div>
+      <Tabs className='tabs-container'
+        inkBarStyle={{ background: 'black' }}
+        tabItemContainerStyle={{width: 600, marginBottom: 10 }}
+        initialSelectedIndex={this.state.tabIdx}
+        value={tabValue}
+        onChange={this._tabSelect}>
+        <Tab icon={
+            <span className='fa-stack fa-lg'>
+              <i className='fa fa-server fa-lg'/>
+            </span>
+          }
+          label='Accounts'
+          value='accounts'
+        >
+          <Accounts account={account}
+            openDia={openAcct}
+            acctClose={this._handleClose} />
+        </Tab>
+        <Tab icon={
+            <span className='fa-stack fa-lg'>
+              <i className='fa fa-phone fa-lg' />
+            </span>
+          }
+          label='Device Search'
+          value='device-search'
+        >
+          <MainView account={account} />
+        </Tab>
+        <Tab icon={
+            <span className='fa-stack fa-lg'>
+              <i className='fa fa-superpowers fa-lg' />
+            </span>
+          }
+          label='Phone Macros'
+          value='templates'
+        >
+          <PhoneMacros />
+        </Tab>
+        <Tab
+          label='BACKGROUNDS'
+          value='backgrounds'
+          icon={
+            <span className='fa-stack fa-lg'>
+              <i className='fa fa-picture-o fa-lg' />
+            </span>
+          }
+        >
+          <Backgrounds account={account} />
+        </Tab>
+      </Tabs>
     );
   }
 }

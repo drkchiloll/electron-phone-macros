830f4968c39becd902957bb982937ff87d79d853
drkchiloll <samwomack_@me.com>
2018-07-06 20:26:52 -0500

    disable strict ssl on certain circumstances

--- src/app/lib/requests.ts
+++ src/app/lib/requests.ts
@@ -1,8 +1,10 @@
-import axios from 'axios';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+import axios, { AxiosInstance } from 'axios';
 
 export const req = (() => {
   const serv = {
     get(options: any) {
+      axios.defaults.adapter = require('axios/lib/adapters/http');
       axios.defaults.validateStatus = status =>
         status >= 200 && status <= 505;
       return axios(options);

77e7b5dd889a23b569884f58727217a28eeda796
drkchiloll <samwomack_@me.com>
2018-07-09 17:37:23 -0700

    added fixed for self signed certificate issues

--- src/app/lib/requests.ts
+++ src/app/lib/requests.ts
@@ -1,13 +1,17 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import axios, { AxiosInstance } from 'axios';
+import { Agent } from 'https'
+
+const request: AxiosInstance = axios.create({
+  adapter: require('axios/lib/adapters/http'),
+  validateStatus: status => status >= 200 && status <= 505,
+  httpsAgent: new Agent({ rejectUnauthorized: false })
+})
 
 export const req = (() => {
   const serv = {
     get(options: any) {
-      axios.defaults.adapter = require('axios/lib/adapters/http');
-      axios.defaults.validateStatus = status =>
-        status >= 200 && status <= 505;
-      return axios(options);
+      return request(options);
     }
   };
   return serv;

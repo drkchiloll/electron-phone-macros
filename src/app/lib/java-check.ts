25a464eb150df5ff794f6cce56cf007714964b6d
drkchiloll <samwomack_@me.com>
2018-07-09 17:41:29 -0700

    check for Java installed on Windows...

---
+++ src/app/lib/java-check.ts
@@ -0,0 +1,54 @@
+import * as targz from 'targz'
+import { Promise, resolve } from 'bluebird';
+import { join } from 'path';
+import { exists } from 'fs';
+import { platform } from 'os';
+import { remote, BrowserWindow } from 'electron';
+
+export const javaChecker = (() => {
+  const service: any = {
+    loadingWindow: BrowserWindow,
+    present() {
+      const path = 'C:\\PhoneMacros\\Java';
+      return new Promise(resolve => {
+        exists(path, (present:boolean) => resolve(present));
+      });
+    },
+    xtractJava() {
+      return new Promise(resolve => {
+        targz.decompress({
+          src: join(__dirname, './jdk-10.0.1.tar.gz'),
+          dest: 'C:\\PhoneMacros\\Java\\'
+        }, (err) => resolve('done'))
+      })
+    },
+    load() {
+      this.loadingWindow = new remote.BrowserWindow({
+        autoHideMenuBar: true,
+        frame: false,
+        alwaysOnTop: true
+      });
+      this.loadingWindow.loadURL(
+        `file://${__dirname}/images/loading.gif`
+      );
+    },
+    run() {
+      console.log(platform())
+      if(platform() !== 'darwin') {
+        return this.present().then(present => {
+          console.log(present)
+          if(!present) {
+            this.load();
+            return this.xtractJava().then(() => {
+              this.loadingWindow.destroy();
+              return 'done';
+            });
+          }
+        });
+      } else {
+        return Promise.resolve('done');
+      }
+    }
+  };
+  return service;
+})();

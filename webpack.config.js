46d109a73c32fa474158141b3b983053383b215a
drkchiloll <samwomack_@me.com>
2018-07-10 06:45:28 -0700

    exclude java and robot from externals

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ let nodeModules = {};
 
 fs.readdirSync('node_modules')
   .filter(function (x) {
-    return ['.bin'].indexOf(x) === -1 && x === 'java'
+    return ['.bin'].indexOf(x) === -1 && (x === 'java' ||
+      x === 'robotjs')
   })
   .forEach(function (mod) {
     nodeModules[mod] = 'commonjs ' + mod;

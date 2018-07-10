d7f8c2173b8ba1beb45118d61dd803a127afb076
drkchiloll <samwomack_@me.com>
2018-07-10 13:56:28 -0700

    Add a JAVA_HOME to the Windows Path Variables

--- src/app/lib/java-check.ts
+++ src/app/lib/java-check.ts
@@ -3,7 +3,8 @@ import { Promise, resolve } from 'bluebird';
 import { join } from 'path';
 import { exists } from 'fs';
 import { platform } from 'os';
-import { remote, BrowserWindow } from 'electron';
+import { exec } from 'child_process';
+import { BrowserWindow } from 'electron';
 
 export const javaChecker = (() => {
   const service: any = {
@@ -29,22 +30,34 @@ export const javaChecker = (() => {
         alwaysOnTop: true
       });
       this.loadingWindow.loadURL(
-        `file://${__dirname}/images/loading.gif`
+        `file://${__dirname}/loading.html`
       );
+    },
+    setHomeAndPath() {
+      return new Promise(resolve => {
+        const home = `setx JAVA_HOME "C:\\PhoneMacros\\Java\\jdk-10.0.1"`,
+          path = `setx PATH "%PATH%;%JAVA_HOME%\\bin`;
+        exec(home, (e, sout, serr) => {
+          exec(path, (e, sout, serror) => resolve('done'))
+        })
+      })
     },
     run() {
       console.log(platform())
       if(platform() !== 'darwin') {
-        return this.present().then(present => {
-          console.log(present)
-          if(!present) {
-            this.load();
-            return this.xtractJava().then(() => {
-              this.loadingWindow.destroy();
-              return 'done';
-            });
-          }
-        });
+        return Promise.all([
+          this.setHomeAndPath(),
+          this.present().then(present => {
+            console.log(present)
+            if(!present) {
+              this.load();
+              return this.xtractJava().then(() => {
+                this.loadingWindow.destroy();
+                return 'done';
+              });
+            }
+          })
+        ]);
       } else {
         return Promise.resolve('done');
       }

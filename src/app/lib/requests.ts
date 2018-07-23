669184cfa85646a7031665bf98a2cab2e7175a16
drkchiloll <samwomack_@me.com>
2018-07-23 09:29:35 -0500

    started writing better error response checking

--- src/app/lib/requests.ts
+++ src/app/lib/requests.ts
@@ -4,19 +4,30 @@ import { Agent } from 'https'
 
 const request: AxiosInstance = axios.create({
   adapter: require('axios/lib/adapters/http'),
-  validateStatus: status => status >= 200 && status <= 505,
+  validateStatus: status => status >= 200 && status <= 599,
   httpsAgent: new Agent({ rejectUnauthorized: false })
 })
 
 export const req = (() => {
   const serv = {
-    handleResponse() {},
+    handleResponse(resp?, err?) {
+      if(resp.status >= 200 && resp.status <= 204)
+        return resp.data;
+      else if(resp.status >= 400 || resp.status <= 599) {
+        if(resp.status === 599) console.log(resp.data);
+        return {
+          error: { status: resp.status, message: resp.statusText }
+        };
+      }
+    },
     get(options: any) {
       return request(options);
     },
     post(options: any) {
       options['method'] = 'post';
-      return request(options);
+      return request(options)
+        .then(this.handleResponse)
+        .catch(this.handleResponse);
     }
   };
   return serv;

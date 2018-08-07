28e88762a5151fd580e779288100bfb5f50e6477
drkchiloll <samwomack_@me.com>
2018-08-07 14:50:10 -0500

    implement Device Report Utility..

--- src/app/components/FeatureButtons.tsx
+++ src/app/components/FeatureButtons.tsx
@@ -1,9 +1,13 @@
 import {
   React, BottomNavigation, BottomNavigationItem,
-  Dialog, RaisedButton, FlatButton, $, fs, Promise
+  Dialog, RaisedButton, FlatButton, $, fs, Promise,
+  Cucm
 } from './index';
 import { ExportMacro } from './ExportMacro';
 import { macroDb } from '../lib/macro-db';
+import { shell } from 'electron';
+import { RisQuery } from 'cucm-risdevice-query';
+import { mainState } from '../lib/main-state';
 
 const styles:any = {
   nav: {
@@ -33,6 +37,7 @@ export class FeatureButtons extends React.Component<any, any> {
       addmacros: false
     }
   }
+  cucm = null;
   _navIcon = cls => {
     return (
       <span className='fa-stack fa-lg' >
@@ -73,6 +78,11 @@ export class FeatureButtons extends React.Component<any, any> {
             icon={this._navIcon('external-link')}
             onClick={this._getMacros}
           />
+          <BottomNavigationItem
+            className='open-report'
+            label='Device Report'
+            icon={this._navIcon('file-text-o')}
+            onClick={this.runSweep} />
         </BottomNavigation>
         { macros ? <ExportMacro cancel={this.cancel} {...this.state} />: null }
         <Dialog
@@ -86,22 +96,103 @@ export class FeatureButtons extends React.Component<any, any> {
               primary={true}
               onClick={() => this.setState({ addmacros: false })}
             />
-          ]}
-        >
-          <RaisedButton
-            label='Add'
-            containerElement='label'
-          >
-            <input
-              name='mymacro'
-              type='file'
-              id='macro-upload'
-              style={styles.file}
-              onChange={this._uploadMacros}
-            />
-          </RaisedButton>
+          ]}>
+            <RaisedButton
+              label='Add'
+              containerElement='label'
+            >
+              <input
+                name='mymacro'
+                type='file'
+                id='macro-upload'
+                style={styles.file}
+                onChange={this._uploadMacros}
+              />
+            </RaisedButton>
         </Dialog>
       </div>
     )
+  }
+  generateQuery = skip => {
+    return `select ${skip ? 'skip ' + skip: ''} first 200 d.name as devicename, ` + 
+      `d.description, n.dnorpattern as primarydn, t.name as model ` + 
+      `from device d\n` +
+      `inner join devicenumplanmap dmap on dmap.fkdevice = d.pkid\n` +
+      `inner join numplan n on dmap.fknumplan = n.pkid\n` +
+      `inner join typemodel t on d.tkmodel = t.enum\n` +
+      `where d.tkclass = 1 AND dmap.numplanindex = 1\n` +
+      `order by t.name`
+  }
+
+  queryRunner = (p: any) => {
+    return this.cucm.query(p.statement).then(results => {
+      p.devices = p.devices.concat(results);
+      if(results.length === 200) {
+        let cquery = this.generateQuery(p.skip);
+        p.skip = p.skip * 2;
+        return this.queryRunner({
+          statement: cquery,
+          skip: p.skip,
+          devices: p.devices
+        })
+      } else {
+        return p.devices;
+      }
+    });
+  }
+
+  deviceHandler = devices => {
+    let dees = [];
+    return Promise.reduce(devices, (a, d: any, index) => {
+      if(a.length === 999) {
+        a.push(d.devicename);
+        dees.push(a);
+        a = [];
+      } else {
+        a.push(d.devicename);
+      }
+      if(index === devices.length - 1) {
+        dees.push(a);
+      }
+      return a;
+    }, []).then(() => dees);
+  }
+
+  runSweep = () => {
+    const { account: { username, password, host, version } } = this.props;
+    let cquery = this.generateQuery(0);
+    let skip: number = 200, devices: any[];
+    let newDevices: any = [];
+    this.cucm = new Cucm({ ...this.props.account });
+    return this.queryRunner({
+      statement: cquery,
+      skip: 200,
+      devices: []
+    }).then(devices => {
+      return this.deviceHandler(devices).then((deviceNames: any[]) => {
+        return Promise.map(deviceNames, (devs: string[]) => {
+          const ris = RisQuery.createRisDoc({
+            version,
+            query: devs,
+            options: { status: undefined }
+          });
+          return this.cucm.risquery({
+            body: ris,
+            bypass: true
+          }).then((risres: any) => {
+            const risos = RisQuery.parseResponse(risres);
+            risos.forEach((d: any) => {
+              const dev = devices.find((device:any) => device.devicename === d.name);
+              d.description = dev.description;
+              d.model = dev.model;
+              newDevices.push(d);
+              return;
+            });
+          })
+        }).then(() => {
+          mainState.createCsv(newDevices, (csv) => shell.openItem(csv));
+        });
+      });
+    })
   }
 }
